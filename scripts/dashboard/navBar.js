4dd2a2a4e62a9606b040b14cb65a424aa57d8e1d
smxky <tallamichael007@gmail.com>
2024-01-02 13:11:13 +0100

    added a pop up for the notification button

--- scripts/dashboard/navBar.js
+++ scripts/dashboard/navBar.js
@@ -1,5 +1,6 @@
 export function navBarButtons(){
     const profileButton = document.querySelector('.js-profile-btn') ;
+    const notificationButton = document.querySelector('.js-notification-btn') ;
 
     profileButton.addEventListener('click', () => {
         if(window.innerWidth > 683){
@@ -14,6 +15,24 @@ export function navBarButtons(){
             document.addEventListener('click', (e) => {
                 if(!document.querySelector('.js-profile-info').contains(e.target) && !profileButton.contains(e.target)){
                     document.querySelector('.js-profile-info').classList.remove('on-profile') ;
+                }
+            }) ;
+        }
+    }) ;
+
+    notificationButton.addEventListener('click', () => {
+        if(window.innerWidth > 683){
+            if(document.querySelector('.js-notification-info').classList.contains('on-profile')){
+                document.querySelector('.js-notification-info').classList.remove('on-profile') ;
+                //document.querySelector('.tool-tip').classList.add('off-tool-tip') ;
+            }else{
+                document.querySelector('.js-notification-info').classList.add('on-profile') ;
+            }
+            //Checking if target element has not been click for the close effect
+
+            document.addEventListener('click', (e) => {
+                if(!document.querySelector('.js-notification-info').contains(e.target) && !notificationButton.contains(e.target)){
+                    document.querySelector('.js-notification-info').classList.remove('on-profile') ;
                 }
             }) ;
         }
